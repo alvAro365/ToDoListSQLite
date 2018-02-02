f57b4101c1a589b23462b7f2337d4c7c7c7e3b96
Alvar Aronija <aronija_1@hotmail.com>
2018-02-02 12:42:57 +0100

    First commit

---
+++ app/src/main/java/com/example/alvar/todolist/CreateToDoActivity.java
@@ -0,0 +1,13 @@
+package com.example.alvar.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_to_do);
+    }
+}
